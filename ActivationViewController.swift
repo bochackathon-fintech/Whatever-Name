9af42a0ed1b8a83ee7ef7cc5cc85493b982dc815
Christos Chadjikyriacou <christos.chadjikyriacou@gmail.com>
2017-06-10 18:16:52 +0300

    initial

---
+++ ActivationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ActivationViewController.swift
+//  DonationGO2
+//
+//  Created by Christos Chadjikyriacou on 10/06/2017.
+//  Copyright © 2017 Christos Chadjikyriacou. All rights reserved.
+//
+
+import UIKit
+
+class ActivationViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    @IBAction func fill(_ sender: Any) {
+        imageView.image = #imageLiteral(resourceName: "activationScreen")
+    }
+
+}
